e7ad3f14aaf36a8c097db4d0982aabf824802c0a
Fivapr <aintensifies@gmail.com>
2018-10-23 20:12:51 +0700

    some new eslint rules, header, withlayout HOC and index.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-	parser: 'babel-eslint',
-	extends: 'airbnb',
-	env: {
-		browser: true,
-		jest: true,
-	},
-	plugins: ['react', 'jsx-a11y', 'import'],
-	rules: {
-		'max-len': ['error', 100],
-		'no-underscore-dangle': ['error', { allow: ['_id'] }],
-		'import/prefer-default-export': 0,
-		'jsx-a11y/anchor-is-valid': 0,
-		'react/react-in-jsx-scope': 0,
-		'react/jsx-filename-extension': [
-			'error',
-			{
-				extensions: ['.js'],
-			},
-		],
-	},
-};
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'max-len': ['error', 100],
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-unused-expressions': 0,
+    semi: 0,
+    // 'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
-[1, 2, 3].map((x) => {
-  const y = x + 1;
-  return x * y;
-});
+import Head from 'next/head'
+import withLayout from '../lib/withLayout'
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
+    </Head>
+    <p>Cлава Украине!</p>
+  </div>
+)
+
+export default withLayout(Index)
